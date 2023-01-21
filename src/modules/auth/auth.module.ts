50377d258375840ed9ab05c8743702b9719fe12b
Domen <domenklep@gmail.com>
2023-01-21 13:10:49 +0100

    Authentication

---
+++ src/modules/auth/auth.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { JwtModule } from '@nestjs/jwt'
+import { PassportModule } from '@nestjs/passport'
+import { UsersModule } from 'modules/users/users.module'
+import { AuthController } from './auth.controller'
+import { AuthService } from './auth.service'
+import { JwtRefreshStrategy } from './strategies/jwt-refresh.strategy'
+import { JwtStrategy } from './strategies/jwt.strategy'
+import { LocalStrategy } from './strategies/local.strategy'
+
+@Module({
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: { expiresIn: `${configService.get('JWT_SECRET_EXPIRES')}` },
+      }),
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, LocalStrategy, JwtStrategy, JwtRefreshStrategy],
+  exports: [AuthService],
+})
+export class AuthModule {}
